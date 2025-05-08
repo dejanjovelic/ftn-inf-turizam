8802dd23b839e9b4ac22c9a4ebd7f5cbd561a804
dejan.jovelic <dejan.jovelic@gmail.com>
2025-05-08 21:02:44 +0200

    feat: Dodata funkcionalnost za dodavanje tura.

--- tourism/app/tours/tour.js
+++ tourism/app/tours/tour.js
@@ -11,45 +11,83 @@ let ture = [];
 
 function createToursRows() {
     let tabela = document.querySelector("#tours-body")
-   
+    tabela.innerHTML = "";
+    if (ture) {
+        for (let i = 0; i < ture.length; i++) {
+            let tr = document.createElement("tr");
 
-    for (let i = 0; i < ture.length; i++) {
-        let tr = document.createElement("tr");
+            let rb = document.createElement("td")
+            let naziv = document.createElement("td");
+            let duzina = document.createElement("td");
 
-        let rb = document.createElement("td")
-        let naziv = document.createElement("td");
-        let duzina = document.createElement("td");
-        
-        rb.textContent = i+1;
-        naziv.textContent = ture[i].naziv;
-        duzina.textContent = ture[i].duzina;
-        
-        tr.appendChild(rb);
-        tr.appendChild(naziv);
-        tr.appendChild(duzina);       
-        tr.addEventListener("click", function(){
-            displayToursDetails(ture[i]);
-        })
-        tabela.appendChild(tr);
+            rb.textContent = i + 1;
+            naziv.textContent = ture[i].naziv;
+            duzina.textContent = ture[i].duzina;
+
+            tr.appendChild(rb);
+            tr.appendChild(naziv);
+            tr.appendChild(duzina);
+            tr.addEventListener("click", function () {
+                displayToursDetails(ture[i]);
+            })
+            tabela.appendChild(tr);
+        }
     }
 }
 function initializationTours() {
-    ture = [
-        new Tura("Pancevo", "Divni grad", 15, ["Grad", "Renesansa", "Restorani"]),
-        new Tura("Novi Sad", "srpska Atina", 15, ["Grad", "Dunav", "Restorani"]),
-        
-    ]
+    ture = JSON.parse(localStorage.getItem("tureJSON"));
     createToursRows();
+    creatingNewTagRowsOnSubmitionForm();
+    handlerFormSubmission();
 }
 document.addEventListener("DOMContentLoaded", initializationTours());
 
-function displayToursDetails(tura){
-    let p  = document.createElement("p");
+function displayToursDetails(tura) {
+    let p = document.createElement("p");
     p.innerHTML = `Naziv: ${tura.naziv} <br><br> Opis: ${tura.opis} <br><br> Dužina ture: ${tura.duzina} km <br><br> Tagovi: ${tura.tagovi.join(", ")}`
 
     let detalji = document.querySelector("#tour-details");
-    if(detalji.firstChild){
+    if (detalji.firstChild) {
         detalji.firstChild.remove();
     }
-    detalji.appendChild(p);}
+    detalji.appendChild(p);
+}
 
+function creatingNewTagRowsOnSubmitionForm() {
+    let buttonAdd = document.querySelector("#addTag");
+    buttonAdd.addEventListener('click', function () {
+        let input = document.createElement("input");
+        input.type = "text";
+        input.name = "tag"
+        input.required = true
+        let unosTaga = document.querySelector("#display-tagRows");
+        unosTaga.appendChild(input);
+    })
+}
+
+
+function handlerFormSubmission() {
+    let submitBtn = document.querySelector("#submitBtn");
+    submitBtn.addEventListener('click', function (event) {
+        const form = document.querySelector("#form");
+        const formData = new FormData(form);
+
+        const naziv = formData.get("naziv");
+        const opis = formData.get("opis");
+        const duzina = formData.get("duzina");
+        const tagovi = formData.getAll("tag")
+
+        const novaTura = new Tura(naziv, opis, duzina, tagovi);
+        if (ture) {
+            ture.push(novaTura);
+        } else {
+            ture = [];
+            ture.push(novaTura);
+        }
+
+        localStorage.setItem("tureJSON", JSON.stringify(ture))
+        createToursRows()
+        let input = document.querySelector("input");
+        
+    })
+}
